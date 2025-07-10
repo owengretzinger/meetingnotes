b9b1fbedd556c07d75dd135533b29ef1034282a6
Owen Gretzinger <owengretzinger@gmail.com>
2025-07-10 12:10:56 -0400

    Initial Commit

---
+++ notetaker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  notetaker
+//
+//  Created by Owen Gretzinger on 2025-07-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ notetaker/notetakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  notetakerApp.swift
+//  notetaker
+//
+//  Created by Owen Gretzinger on 2025-07-10.
+//
+
+import SwiftUI
+
+@main
+struct notetakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
